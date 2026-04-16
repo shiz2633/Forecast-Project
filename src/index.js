c8df9efac62a491917c538ea1a6549c7aad7e745
shiz2633 <2021s.kiani@gmail.com>
2026-04-16 17:25:17 +0100

    changed CSS LAYOUT
    changed the layout of CSS and HTML few changes to push weather and details of weather left and right ADDED CLASSES

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
+function refreshTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "534da33b5ce367f43atb4e240d7d01o0";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshTemperature);
+}
 document.addEventListener("DOMContentLoaded", function () {
   function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
     let cityElement = document.querySelector("#city");
     cityElement.innerHTML = searchInput.value;
+    searchCity(searchInput.value);
   }
 
   let searchFormElement = document.querySelector("#search-form");
